9dc573c0534b73ea0c152d678250f2d770b32934
carajillu <joan.clark88@gmail.com>
2025-03-11 15:51:00 +0000

    python template prototytpes

--- pycp2k/templates/template_global.py
+++ pycp2k/templates/template_global.py
@@ -1,18 +1,27 @@
 from pycp2k import CP2K
+import os
 class TemplateGlobal(CP2K):
+    template="""&GLOBAL
+                   PROJECT_NAME TEMPLATE
+                   PRINT_LEVEL MEDIUM
+                &END GLOBAL"""
     print_levels = {"SILENT","LOW", "MEDIUM", "HIGH", "DEBUG"}
-    def __init__(self,template_filein):
+
+    def __init__(self,template_filein:str=None):
         super().__init__()
+
+        if template_filein is None:
+            template_filein="template_global.in"
+            with open(template_filein,"w") as f:
+                f.write(template)
         self.parse(template_filein)
+        os.remove("template_global.in")
+
         if self.CP2K_INPUT.GLOBAL.Print_level not in self.print_levels:
            raise ValueError(f"Invalid print level: {self.CP2K_INPUT.GLOBAL.Print_level}")
         
-    def set_print_level(self, calc: CP2K):
-        if hasattr(self, "CP2K_INPUT") and hasattr(self.CP2K_INPUT, "GLOBAL"):
-           calc.CP2K_INPUT.GLOBAL.Print_level = self.CP2K_INPUT.GLOBAL.Print_level
-        else:
-           print("TemplateGlobal object has no PRINT_LEVEL value set. Setting PRINT_LEVEL to MEDIUM as CP2K default")
-           calc.CP2K_INPUT.GLOBAL.Print_level="MEDIUM"
+    def assign_section(self,calc:CP2K):
+        GLOBAL=self.CP2K_INPUT.GLOBAL
+        calc.CP2K_INPUT.GLOBAL=GLOBAL
+        
     
-    def set_project_name(self, calc: CP2K):
-        calc.CP2K_INPUT.GLOBAL.Project_name=self.calc.CP2K_INPUT.GLOBAL.Project_name
