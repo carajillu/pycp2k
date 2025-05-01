85246d3df27eb6befcf4f3a435a6158d201f0f19
carajillu <joan.clark88@gmail.com>
2025-05-01 10:23:16 +0100

    scf loops added explicitly

--- pycp2k/templates/FORCE_EVAL/xTB_templates.py
+++ pycp2k/templates/FORCE_EVAL/xTB_templates.py
@@ -1,13 +1,16 @@
 from pycp2k.templates.GLOBAL.GLOBAL import CP2K
 from pycp2k.templates.FORCE_EVAL.DFT.xTB import add_xTB
-from pycp2k.templates.FORCE_EVAL.SCF.OT import add_SCF_OT
+from pycp2k.templates.FORCE_EVAL.SCF.OT import add_OT
+from pycp2k.templates.FORCE_EVAL.SCF.loop import add_inner_scf,add_outer_scf
 from pycp2k.templates.FORCE_EVAL.SUBSYS.add_atoms import add_coords,add_cell
 from ase import Atoms
 
 def add_xTB_OT(atoms:Atoms,calc:CP2K,**kwargs):
     calc.CP2K_INPUT.FORCE_EVAL_add()
     add_xTB(atoms=atoms,calc=calc,**kwargs)
-    add_SCF_OT(calc=calc,**kwargs)
+    add_OT(calc=calc,**kwargs)
+    add_inner_scf(calc=calc,**kwargs)
+    add_outer_scf(calc=calc,**kwargs)
     add_coords(atoms=atoms,calc=calc)
     add_cell(atoms=atoms,calc=calc)
     return
