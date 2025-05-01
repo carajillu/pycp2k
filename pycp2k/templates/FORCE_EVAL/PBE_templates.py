e80ccba8e1a19dac9547637af23ed31bc11c4dba
carajillu <joan.clark88@gmail.com>
2025-05-01 15:43:54 +0100

    First PBE templates

---
+++ pycp2k/templates/FORCE_EVAL/PBE_templates.py
@@ -0,0 +1,22 @@
+from pycp2k.templates.GLOBAL.GLOBAL import CP2K
+from pycp2k.templates.FORCE_EVAL.DFT.PBE import add_PBE,del_PBE
+from pycp2k.templates.FORCE_EVAL.SCF.OT import add_OT
+from pycp2k.templates.FORCE_EVAL.SCF.loop import add_inner_scf,add_outer_scf
+from pycp2k.templates.FORCE_EVAL.SUBSYS.add_atoms import add_coords,add_cell
+from pycp2k.templates.FORCE_EVAL.SUBSYS.add_kinds import add_kinds
+from ase import Atoms
+
+def add_PBE_OT(atoms:Atoms,calc:CP2K,**kwargs):
+    calc.CP2K_INPUT.FORCE_EVAL_add()
+    add_PBE(calc=calc,**kwargs)
+    add_OT(calc=calc,**kwargs)
+    add_inner_scf(calc=calc,**kwargs)
+    add_outer_scf(calc=calc,**kwargs)
+    add_coords(atoms=atoms,calc=calc)
+    add_cell(atoms=atoms,calc=calc)
+    add_kinds(atoms=atoms,calc=calc,**kwargs)
+    return
+
+def del_PBE_OT(calc:CP2K):
+    raise NotImplementedError("Not implemented")
+    return
