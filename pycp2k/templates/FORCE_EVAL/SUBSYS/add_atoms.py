2dce29f4af974b2b708877e56b69b706f72a8d78
carajillu <joan.clark88@gmail.com>
2025-05-07 09:34:17 +0100

    multiplying coordinates by 10 to convert to angstrom (ase does nm)

--- pycp2k/templates/FORCE_EVAL/SUBSYS/add_atoms.py
+++ pycp2k/templates/FORCE_EVAL/SUBSYS/add_atoms.py
@@ -6,7 +6,7 @@ def add_coords(atoms:Atoms,calc:CP2K):
     #Adds the coordinates section to a calculator containing a SUBSYS section
     default_keyword=[]
     for i in range(len(atoms)):
-        default_keyword.append(atoms[i].symbol+" "+" ".join(map(str, atoms.positions[i])))
+        default_keyword.append(atoms[i].symbol+" "+" ".join(map(str, atoms.positions[i]*10))) # convert to angstroms
     calc.CP2K_INPUT.FORCE_EVAL_list[0].SUBSYS.COORD.Default_keyword=default_keyword
     return
 
@@ -15,8 +15,8 @@ def add_cell(atoms:Atoms,calc:CP2K):
     if not atoms.cell:
        # find maximum and minimum coordinates in each direction.
        atoms.center()
-       max_coords=atoms.positions.max(axis=1)
-       min_coords=atoms.positions.min(axis=1)
+       max_coords=atoms.positions.max(axis=1)*10 # convert to angstroms
+       min_coords=atoms.positions.min(axis=1)*10 # convert to angstroms
        cell_lengths=max_coords-min_coords
        atoms.cell=np.array([[cell_lengths[0],0,0],[0,cell_lengths[1],0],[0,0,cell_lengths[2]]])
     calc.CP2K_INPUT.FORCE_EVAL_list[0].SUBSYS.CELL.A=" ".join(map(str, atoms.cell[0]))
