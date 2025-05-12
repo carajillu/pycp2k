39357d2b173550173666682ed8d392bea567e063
carajillu <joan.clark88@gmail.com>
2025-05-12 12:24:11 +0100

    Changed default SMEAR_Electronic_temperature to 300.0

--- pycp2k/templates/FORCE_EVAL/DFT/SCF/smear.py
+++ pycp2k/templates/FORCE_EVAL/DFT/SCF/smear.py
@@ -4,7 +4,7 @@ def add_smear(calc:CP2K,**kwargs):
     SCF=calc.CP2K_INPUT.FORCE_EVAL_list[0].DFT.SCF
     SCF.Added_mos=kwargs.get("SCF_Added_mos","10 10")
     SCF.SMEAR.Method=kwargs.get("SMEAR_Method","FERMI_DIRAC")
-    SCF.SMEAR.Electronic_temperature=kwargs.get("SMEAR_Electronic_temperature",0.0)
+    SCF.SMEAR.Electronic_temperature=kwargs.get("SMEAR_Electronic_temperature",300.0)
     print(f"CP2K_INPUT.FORCE_EVAL_list[0].DFT.SCF.SMEAR.Method: {SCF.SMEAR.Method}")
     print(f"CP2K_INPUT.FORCE_EVAL_list[0].DFT.SCF.SMEAR.Electronic_temperature: {SCF.SMEAR.Electronic_temperature}")
     return
