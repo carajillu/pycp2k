1333c6eaeb19e3c87adc1d8401017e7f91edbdda
carajillu <joan.clark88@gmail.com>
2025-06-17 09:34:42 +0100

    add_xTB() now accepts charge and LSD as parametters

--- pycp2k/templates/FORCE_EVAL/DFT/xTB.py
+++ pycp2k/templates/FORCE_EVAL/DFT/xTB.py
@@ -1,13 +1,14 @@
 from pycp2k.templates.GLOBAL.GLOBAL import CP2K
 
-def add_xTB(calc:CP2K,**kwargs):
-    feval_idx=kwargs.get("feval_idx",0)
+def add_xTB(calc:CP2K,feval_idx:int=0,charge:int=0,LSD:bool=False,**kwargs):
     if  len(calc.CP2K_INPUT.FORCE_EVAL_list) <= feval_idx:
         raise ValueError(f"FORCE_EVAL section {feval_idx} not found in calculator")
 
-    print(f"===Adding xTB to FORCE_EVAL section {feval_idx}===")
+    print(f"=== Adding xTB to FORCE_EVAL section {feval_idx} ===")
     calc.CP2K_INPUT.FORCE_EVAL_list[feval_idx].Method="QS"
     DFT=calc.CP2K_INPUT.FORCE_EVAL_list[feval_idx].DFT
+    DFT.Charge=charge
+    DFT.Lsd=LSD
     DFT.QS.Method="XTB"
     DFT.QS.XTB.Check_atomic_charges=kwargs.get("check_atomic_charges",False)
     DFT.QS.XTB.Do_ewald=kwargs.get("do_ewald",True)  
@@ -15,6 +16,7 @@ def add_xTB(calc:CP2K,**kwargs):
     print(f"check_atomic_charges = {DFT.QS.XTB.Check_atomic_charges}")
     print(f"do_ewald = {DFT.QS.XTB.Do_ewald}")
     print(f"use_halogen_correction = {DFT.QS.XTB.Use_halogen_correction}")
+    print(f"=== xTB added to FORCE_EVAL section {feval_idx} ===")
     return
 
 def del_xTB(calc:CP2K):
