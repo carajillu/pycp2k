bb54088d2bda8b38f9eddc34bcda7e527b490a11
carajillu <joan.clark88@gmail.com>
2025-10-27 10:54:20 +0000

    config.py is generated by install script. Needs to go and added to .gitignore

--- src/pycp2k/config.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-cp2k_default_command = "cp2k.psmp"
-mpi_default_command = "mpirun"
-mpi_on_default = True
-build_version = "2025.1"
-build_revision = "Version)"
