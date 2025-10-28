fd1e2cdbc756a5ff3cff6b3c6d577e9810b48286
carajillu <joan.clark88@gmail.com>
2025-10-28 12:31:52 +0000

    Added the templates.GLOBAL.CP2K.atoms attribute with a setter that sets the SUBSYS section of the calculator

--- src/pycp2k/templates/GLOBAL/GLOBAL.py
+++ src/pycp2k/templates/GLOBAL/GLOBAL.py
@@ -1,11 +1,14 @@
+import os
 import pycp2k
 from pycp2k import CP2K
-import os
+from pycp2k.templates.FORCE_EVAL.SUBSYS.add_atoms import add_coords, add_cell
+from pycp2k.templates.FORCE_EVAL.SUBSYS.add_kinds import add_kinds
 class CP2K(CP2K):
 
     def __init__(self,project_name:str="TEMPLATE",print_level:str="MEDIUM",run_type:str="ENERGY",\
                  working_directory:str=None,cp2k_command:str="cp2k.psmp"):
         super().__init__()
+        self._atoms=None
         self.working_directory=working_directory
         self.project_name=project_name
         self.cp2k_command=cp2k_command
@@ -23,6 +26,19 @@ class CP2K(CP2K):
         print(f"CP2K_INPUT.GLOBAL.Project_name: {self.CP2K_INPUT.GLOBAL.Project_name}")
         print(f"CP2K_INPUT.GLOBAL.Print_level: {self.CP2K_INPUT.GLOBAL.Print_level}")
         print(f"CP2K_INPUT.GLOBAL.Run_type: {self.CP2K_INPUT.GLOBAL.Run_type}")
+    
+    @property
+    def atoms(self):
+        return self._atoms
+    @atoms.setter
+    def atoms(self,atoms):
+        self._atoms=atoms
+        if len(self.CP2K_INPUT.FORCE_EVAL_list)==0:
+           self.CP2K_INPUT.FORCE_EVAL_add()
+        add_coords(atoms,self)
+        add_cell(atoms,self)
+        add_kinds(atoms,self)
+
     
     def cleanup(self,quiet:bool=False):
         
