ef894bacd42663b966c1583be2c45aa1d9fcd4e1
carajillu <joan.clark88@gmail.com>
2025-11-07 16:08:08 +0000

    Added functionality to load a cp2k input file when creating the calculator

--- src/pycp2k/templates/GLOBAL/GLOBAL.py
+++ src/pycp2k/templates/GLOBAL/GLOBAL.py
@@ -6,16 +6,20 @@ from pycp2k.templates.FORCE_EVAL.SUBSYS.add_atoms import add_coords, add_cell
 from pycp2k.templates.FORCE_EVAL.SUBSYS.add_kinds import add_kinds
 class CP2K(CP2K):
 
-    def __init__(self,project_name:str="TEMPLATE",print_level:str="MEDIUM",run_type:str="ENERGY",\
+    def __init__(self,input_file:str=None,project_name:str="TEMPLATE",print_level:str="MEDIUM",run_type:str="ENERGY",\
                  working_directory:str=None,cp2k_command:str="cp2k.psmp",mpi_n_procs: int=None):
         super().__init__()
         self._atoms=None
         self.working_directory=working_directory
-        self.project_name=project_name
         self.cp2k_command=cp2k_command
-        self.CP2K_INPUT.GLOBAL.Project_name=project_name
-        self.CP2K_INPUT.GLOBAL.Print_level=print_level
-        self.CP2K_INPUT.GLOBAL.Run_type=run_type
+        if input_file is not None:
+           self.parse(input_file)
+           self.project_name=self.CP2K_INPUT.GLOBAL.Project_name
+        else:
+           self.project_name=project_name
+           self.CP2K_INPUT.GLOBAL.Project_name=project_name
+           self.CP2K_INPUT.GLOBAL.Print_level=print_level
+           self.CP2K_INPUT.GLOBAL.Run_type=run_type
         self.version=float(pycp2k.config.build_version)
         self.revision=pycp2k.config.build_revision
         self.working_directory=working_directory
@@ -40,9 +44,17 @@ class CP2K(CP2K):
         self._atoms=atoms
         if len(self.CP2K_INPUT.FORCE_EVAL_list)==0:
            self.CP2K_INPUT.FORCE_EVAL_add()
+        elif len(self.CP2K_INPUT.FORCE_EVAL_list)>1:
+           raise "Can't set atoms for a calculator with more than one FORCE_EVAL section" 
+        try:
+            del(self.CP2K_INPUT.FORCE_EVAL_list[0].SUBSYS.TOPOLOGY)
+            del(self.CP2K_INPUT.FORCE_EVAL_list[0].SUBSYS.__dict__["_subsections"]["TOPOLOGY"])
+        except Exception as e:
+            print(e)
+            print ("If this is a AttributeError or a KeyError, it's just trying to delete a TOPOLOGY section that's not there and it's harmless")
         add_coords(atoms,self)
         add_cell(atoms,self)
-        add_kinds(atoms,self)
+        #add_kinds(atoms,self) #kinds should be managed differently
     @atoms.deleter
     def atoms(self):
         del(self._atoms)
