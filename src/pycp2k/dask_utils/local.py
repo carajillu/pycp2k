80e72ffc5e3ca2d7fa2763beb7158299b28150d0
carajillu <joan.clark88@gmail.com>
2025-11-07 16:07:34 +0000

    For external binaries, we use n_workers=1 and threads_per_worker=1 and handle the number of concurrent tasks with cluster.scale()

--- src/pycp2k/dask_utils/local.py
+++ src/pycp2k/dask_utils/local.py
@@ -1,4 +1,10 @@
 from dask.distributed import Client, LocalCluster
-def create_cluster(n_workers: int=4, threads_per_worker: int=2):
+def create_cluster(n_workers: int=1, threads_per_worker: int=1,scale=1):
+    """
+    create a Dask local cluster. For external binaries, we use 
+    n_workers=1 and threads_per_worker=1 and handle the number of
+    concurrent tasks with cluster.scale()
+    """
     cluster = LocalCluster(n_workers=n_workers, threads_per_worker=threads_per_worker)
+    cluster.scale(scale)
     return cluster
